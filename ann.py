8e9361e7bfc4f02df259baeb40e5d4ae7b29397a
SIVADAS P R <sivadasrajan@gmail.com>
2023-06-01 01:45:34 +0530

    phase 2

--- ann.py
+++ ann.py
@@ -1,72 +1,98 @@
-import random 
-from math import exp
-from math import pow
-class Layer:
-   
-    def __init__(self,nodes_in = 1,nodes_out = 1):
-        self.nodes_in = nodes_in
-        self.nodes_out = nodes_out
-        self.inputs  = []
-        self.weights = []
-        self.biases = []
-        self.outputs = []
-        self.activations = []
-        for i in range(0,nodes_in*nodes_out):
-            self.weights.append(random.uniform(-1, 1))
-        for i in range(0,nodes_out):
-            self.biases.append(random.uniform(-1, 1))
-            self.outputs.append(None)
-            self.activations.append(None)
+#https://github.com/tensorflow/playground/blob/master/src/nn.ts#L358
+import random
+import math
+def activation_sigmoid(x):
+    return 1/(1+math.exp(-x))
 
- 
-    def calculate_output(self,inputs):
-        weighted_outs = []
+class Link:
+    def __init__(self, src, dst):
+        self.weight = random.uniform(-1, 1)
+        self.src:Node = src
+        self.dst:Node = dst
+        self.err_derivative: float = 0
 
-        for i in range(0,self.nodes_out):
-            for i in range(0,self.nodes_in):
+class Node:
+    def __init__(self,):
+        self.bias: float = 0.5
+        self.input_links: Link = []
+        self.output_links: Link = []
+        
+        # sum(weights*input) + bias
+        self.total_input: float = 0
+
+        #activation(total_input)
+        self.output: float = 0
+
+        self.input_derivative: float = 0
+        self.output_derivative: float = 0
+        
+    def update_output(self):
+        self.total_input = self.bias
+        for link in self.input_links:
+            self.total_input += link.weight*link.src.output
+        self.output = activation_sigmoid(self.total_input)
+        return self.output
+
+class Network:
+    def __init__(self):
+        pass
+
+    def buildNetwork(self, network_size: list):
+        network = []
+        for i in range(0, len(network_size)):
+
+            is_output_layer = i == len(network_size)-1
+            is_input_layer = i == 0
+            layer = []
+
+            for j in range(0, network_size[i]):
+                new_node = Node()
+                if i >= 1:
+                    prevLayer = network[i-1]
+                    for prev_node in prevLayer:
+                        link = Link(prev_node, new_node)
+                        new_node.input_links.append(link)                        
+                        prev_node.output_links.append(link)
+                layer.append(new_node)
+            network.append(layer)
+        self.network = network
+
+    def print_network(self):
+        for i,layer in enumerate(self.network):
+            print("Layer",i)
+            for j,node in enumerate(layer):
+                print("\tNode",i,j)
+                print("\t\tBias:",node.bias)
+                print("\t\tOutput Weights: ",end='')
+                for w in node.output_links:
+                    print(round(w.weight,2),end=',')
+                print("")
+                print("\t\tInput Weights : ",end='')
+                for w in node.input_links:
+                    print(round(w.weight,2),end=',')
+                print("")
+                print("\t\tOutput: ",node.total_input)
+                print("\t\tActivation: ",node.output)
 
 
-        for 
-        self.print_outputs()
-        return self
-
-    def get_weight(self,node_in,node_out):
-        return self.weights[node_out * self.n]
-
-    def sigmoid(self,z):
-        return 1 / (1 + exp(-1 * z))
-
-    def print_outputs(self):
-        print('[',end='')
-        for x in self.outputs:
-            print(round(x,2),end=',')
-        print(']',end='\n')
-
-    def mse(self,one_hot):
-        loss = []
-        for i,output in enumerate(self.outputs):
-            loss.append(pow(one_hot[i] - output,2))
-        return loss #/ len(one_hot)
-    
-    def gradient_for_weight(self,one_hot):
-        gradient = []
-        for i,weight in enumerate(self.weights):
-            temp = (2/len(one_hot)) * (one_hot[i] - self.outputs[i])
-            temp *= (self.activations[i]*(1-self.activations[i]))
-            gr1.append(temp)
-        return  gr1
-
-    
 
 
-with open('iris-mini.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print("row----------")
-        print([float(row['sepal_length']),float(row['sepal_width']),float(row['petal_length']),float(row['petal_width'])])
-        input_layer = [float(row['sepal_length']),float(row['sepal_width']),float(row['petal_length']),float(row['petal_width'])]
-        h1 = Layer(10).calculate_output(input_layer)
-        h2 = Layer(15).calculate_output(h1.outputs)
-        h3 = Layer(3).calculate_output(h2.outputs)
+    def forward_propagate(self,inputs:list):
+        input_layer = self.network[0]
+        
+        if len(input_layer) != len(inputs):
+            raise Exception("The number of inputs must match the number of nodes in the input layer")
+        
+        for i,node in enumerate(input_layer):
+            node.output = inputs[i]
+        for i in range(1,len(self.network)):
+            current_layer = self.network[i]
+            for j,node in enumerate(current_layer):
+                node.update_output()
+        
 
-        h3.outputs 
+
+x  = Network()
+x.buildNetwork([2,2,1])
+x.forward_propagate([5,5])
+x.print_network()
